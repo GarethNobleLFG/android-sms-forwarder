d2d8eb3fb087426a8ca02b4f91ace53ec6ad64dd
Gareth Noble <garethnoble3@gmail.com>
2026-01-28 16:07:31 -0500

    refactor: moved sms service code to its own file

--- desktop-overlay/main.js
+++ desktop-overlay/main.js
@@ -1,45 +1,10 @@
 require('dotenv').config();
 
 const { app, BrowserWindow, screen } = require('electron');
-const axios = require('axios');
-const ContactManager = require('./services/contacts');
+const checkForMessages = require('./services/smsService');
 
 app.disableHardwareAcceleration();
 app.commandLine.appendSwitch('--disable-http-cache');
-
-
-
-
-// Initialize contact manager
-const contactManager = new ContactManager();
-
-
-
-
-
-// Check for new SMS messages every 2 seconds.
-function checkForMessages() {
-    setInterval(async () => {
-        try {
-            const response = await axios.get(`${process.env.API_URL}/sms-api/latest`);
-
-            const newSmsChats = response.data.messages || [];
-
-            for (let i = 0; i < newSmsChats.length; i++) {
-                console.log('New SMS found:', newSmsChats[i].sender);
-
-                // Get contact name or just pass number if there is none.
-                const contactName = contactManager.getContactName(newSmsChats[i].sender);
-                showSMS(contactName, newSmsChats[i].message);
-            }
-        }
-        catch (error) {
-            console.log('Cannot connect to SMS API. :(');
-        }
-    }, 500);
-}
-
-
 
 
 
@@ -122,8 +87,9 @@ app.whenReady().then(() => {
         showSMS('System', 'Waiting for SMS messages...');
     }, 1000);
 
-    checkForMessages();
-    
+    // Call SMS service from services that will run on intervals. Pass showSMS function to apply new events.
+    checkForMessages(showSMS);
+
     console.log('SMS Desktop Overlay is running!');
 });
 
