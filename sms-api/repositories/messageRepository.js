aca0af6e6da00f3d8a91ed17bc6e65f50654a006
Gareth Noble <garethnoble3@gmail.com>
2026-05-14 22:37:43 -0400

    fix: finding unread repository function now returns icon image

--- sms-api/repositories/messageRepository.js
+++ sms-api/repositories/messageRepository.js
@@ -7,18 +7,17 @@ const create = async (data) => {
 const findUnread = async (dateString) => {
     // Get bounds for the requested date since the return timestamp is a Unix Epoch Timestamp.
     const targetDate = new Date(dateString);
-    
+
     targetDate.setHours(0, 0, 0, 0);
     const startTimestamp = targetDate.getTime();
-    
+
     targetDate.setHours(23, 59, 59, 999);
     const endTimestamp = targetDate.getTime();
 
-    // Fetch from MongoDB
     return await Messages.find({
         timestamp: { $gte: startTimestamp, $lte: endTimestamp },
         read: false
-    }).select('_id app_package title message image_base64 read timestamp');
+    }).select('_id app_package title message image_base64 icon_base64 read timestamp');
 };
 
 const markAsRead = async (ids) => {
