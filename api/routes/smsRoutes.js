bad0811df7399eea45471935b159f747b8ff5985
Gareth Noble <garethnoble3@gmail.com>
2026-05-14 23:27:14 -0400

    docs: edited comments

--- api/routes/smsRoutes.js
+++ api/routes/smsRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const smsController = require('../controller/smsController');
 
-// POST - Save new Notification message
+// POST - Save new Notification message.
 router.post('/save', smsController.saveMessage);
 
-// GET - Get latest messages
+// GET - Get latest messages.
 router.get('/latest', smsController.getLatestMessages);
 
-// POST - Mark specific messages as read
+// POST - Mark specific messages as read.
 router.post('/read', smsController.markAsRead);
 
-// GET - Wipe database of all data
+// GET - Wipe database of all data.
 router.get('/delete-db', smsController.deleteDb);
 
 module.exports = router;
